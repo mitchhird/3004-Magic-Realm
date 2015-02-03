285d7aa2eba7a8168a26b3575210170557cb4e26
Anthony Tasca <atasca10@gmail.com>
2015-02-03 14:37:42 -0500

    Skeleton for UI class

---
+++ src/views/boardView.java
@@ -0,0 +1,16 @@
+package views;
+
+import javax.swing.JFrame;
+
+public class boardView extends JFrame {
+
+	private static final long serialVersionUID = -3255182183312639441L;
+	
+	public boardView(){
+		init();
+	}
+	
+	public void init(){
+		
+	}
+}
