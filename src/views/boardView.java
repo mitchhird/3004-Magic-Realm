585a7a6e1b3bb5eb2b3936892b18fac565752b5e
Anthony Tasca <atasca10@gmail.com>
2015-02-04 14:29:55 -0500

    Testing commit on Desktop

--- src/views/boardView.java
+++ src/views/boardView.java
@@ -6,6 +6,10 @@ public class boardView extends JFrame {
 
 	private static final long serialVersionUID = -3255182183312639441L;
 	
+	public static void main(String args[]){
+		new boardView();
+	}
+	
 	public boardView(){
 		init();
 	}
