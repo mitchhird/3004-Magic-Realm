48daf13e0a5d5714ead6731d929ab8d644e16f63
Mitchell <mitchhird@gmail.com>
2015-02-11 01:08:46 -0500

    Adding in convience functions that will make calls eaiser

--- src/models/characterModels/playerEnums/CharacterClass.java
+++ src/models/characterModels/playerEnums/CharacterClass.java
@@ -1,5 +1,10 @@
 package models.characterModels.playerEnums;
 
+import java.awt.Image;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 /**
  * A Basic Enumeration That Will Allow For Character Specific 
  * Functionality If Needed
@@ -11,5 +16,31 @@ public enum CharacterClass {
    CAPTAIN,
    DWARF,
    ELF,
-   SWORDSMAN
+   SWORDSMAN;
+   
+   // Method That Will Return The Ready Tile For The Class
+   public Image getReadyTile () {
+	   String baseDir = System.getenv("user.dir");
+		try {
+			switch (this) {
+			case AMAZON:
+				return ImageIO.read(new File(baseDir + "/images/characters","amazon.png"));
+			case BLACKNIGHT:
+				return ImageIO.read(new File(baseDir + "/images/characters","black_knight.png"));
+			case CAPTAIN:
+				return ImageIO.read(new File(baseDir + "/images/characters","captain.png"));
+			case DWARF:
+				return ImageIO.read(new File(baseDir + "/images/characters","dwarf.png"));
+			case ELF:
+				return ImageIO.read(new File(baseDir + "/images/characters","elf.png"));
+			case SWORDSMAN:
+				return ImageIO.read(new File(baseDir + "/images/characters","swordsman.png"));
+			default:
+				return ImageIO.read(new File(baseDir + "/images/characters","amazon.png"));
+			}
+	   } catch (Exception e) {
+		   e.printStackTrace();
+	   }
+		return null;
+   }
 }
