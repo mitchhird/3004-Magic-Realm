ed53de51d64b4a891e72c74af0caa4e13c6a2fb9
Mitchell <mitchhird@gmail.com>
2015-02-22 17:03:17 -0500

    Adding In Main App

---
+++ src/app/AppMagicRealm.java
@@ -0,0 +1,14 @@
+package app;
+
+import views.GameView;
+
+/**
+ * Main Class For The Magic Realm Game. Creates A Game View And Starts Everything
+ * @author Mitchell
+ */
+public class AppMagicRealm {
+	// Main Method To Launch The Java App
+	public static void main (String[] args) {
+		new GameView();
+	}
+}
--- src/app/Readme.java
+++
@@ -1,5 +0,0 @@
-package app;
-
-public class Readme {
-	// APPS GO HEAR!!!
-}
