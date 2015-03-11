6a5ac9d8ed0404bdf1cc8d9f4b1432b02efdba15
Anthony Tasca <atasca10@gmail.com>
2015-03-11 14:39:28 -0400

    refactored the rollview components to have better naming, added action listeners to rollview

--- src/views/RollView.java
+++ src/views/RollView.java
@@ -1,33 +1,36 @@
 package views;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class RollView extends javax.swing.JFrame {
 
 	private static final long serialVersionUID = 6220875929681965271L;
-	private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+	private javax.swing.JButton setButton;
+    private javax.swing.JButton randomizeButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JTextField jTextField1;    
+    private javax.swing.JLabel maxRollLabel;
+    private javax.swing.JTextField desiredRollField;    
     
     private int maxRollValue;
     
     public RollView(int maxRoll) {
         maxRollValue = maxRoll;
     	initComponents();
+    	addListeners();
     }
     
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        setButton = new javax.swing.JButton();
+        randomizeButton = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        desiredRollField = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        maxRollLabel = new javax.swing.JLabel();
         
         addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent e){
@@ -35,18 +38,18 @@ public class RollView extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("Set");
+        setButton.setText("Set");
 
-        jButton2.setText("Randomize");
+        randomizeButton.setText("Randomize");
 
         jLabel1.setText("Desired Roll Value:");
 
-        jTextField1.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
-        jTextField1.setText("0");
+        desiredRollField.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        desiredRollField.setText("0");
 
         jLabel2.setText("Maximum Roll Value:");
 
-        jLabel3.setText(Integer.toString(maxRollValue));
+        maxRollLabel.setText(Integer.toString(maxRollValue));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -58,15 +61,15 @@ public class RollView extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(desiredRollField, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
+                        .addComponent(setButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2))
+                        .addComponent(randomizeButton))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel3)
+                        .addComponent(maxRollLabel)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -76,17 +79,43 @@ public class RollView extends javax.swing.JFrame {
                 .addContainerGap(12, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jLabel3))
+                    .addComponent(maxRollLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(desiredRollField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(setButton)
+                    .addComponent(randomizeButton))
                 .addContainerGap())
         );
         pack();
-    }                      
+    }   
+    
+    private void addListeners() {
+    	randomizeButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				handleRandomizeButton();
+			}
+		});
+    	
+    	setButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				handleSetButton();
+			}
+		});
+    }
+
+	private void handleSetButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void handleRandomizeButton() {
+		// TODO Auto-generated method stub
+		
+	}
 }
