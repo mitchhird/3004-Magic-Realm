e37fe7e66d3c76e0d8ecda43c0b8dbad99026890
Anthony Tasca <atasca10@gmail.com>
2015-03-16 15:43:15 -0400

    added giant and monster base model & put giant on the board

---
+++ src/models/monsterModels/Giant.java
@@ -0,0 +1,20 @@
+package models.monsterModels;
+
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Giant extends MonsterBase {
+	
+	public Giant(){
+		super();
+		monsterType = "Giant";
+		try {
+			monsterImage = ImageIO.read(getClass().getResource("/monsters_c/giant.gif"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/models/monsterModels/MonsterBase.java
@@ -0,0 +1,13 @@
+package models.monsterModels;
+
+import java.awt.Image;
+
+public class MonsterBase {
+	
+	protected String monsterType;
+	protected Image monsterImage;
+	
+	public Image getImage(){
+		return monsterImage;
+	}
+}
