aae4aab254932d0ec16e1a26769613a995d97e8f
Mitchell <Mitchell@172.17.69.76>
2015-03-17 15:08:00 -0700

    Tile update and all that jazz

--- src/models/BoardModels/Tile.java
+++ src/models/BoardModels/Tile.java
@@ -1,5 +1,8 @@
 package models.BoardModels;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import models.characterModels.playerEnums.TileType;
 
 public class Tile {
@@ -10,85 +13,28 @@ public class Tile {
 	 * and if its a mountains caves valley or forest
 	 */
 	
-	private Clearing clear1;
-	private Clearing clear2;
-	private Clearing clear3;
-	private Clearing clear4;
-	private Clearing clear5;
-	private Clearing clear6;
+	private Set <Clearing> clearings;
 	private TileType type;
 	
 	public Tile(){
-		clear1 = null;
-		clear2 = null;
-		clear3 = null;
-		clear4 = null;
-		clear5 = null;
-		clear6 = null;
+		type = null;
+		clearings = new HashSet<Clearing>();
 	}
 	
 	// These can be null if not there
-	public Tile(Clearing c1, Clearing c2, Clearing c3, 
-			Clearing c4, Clearing c5, Clearing c6, TileType tile){
-		addClearing(1, c1);
-		addClearing(2, c3);
-		addClearing(3, c3);
-		addClearing(4, c4);
-		addClearing(5, c5);
-		addClearing(6, c6);
+	public Tile(TileType tile, Clearing ... clearings){
 		type = tile;
+		this.clearings = new HashSet<Clearing>();
+		
+		// Add everything In
+		for (Clearing c: clearings) {
+			this.clearings.add(c);
+		}
 	}
 	
 	public TileType getType(){
 		return type;
 	}
 	
-	public void addClearing(int num, Clearing clear){
-		switch(num){
-		case 1:
-			clear1 = clear;
-			break;
-		case 2:
-			clear2 = clear;
-			break;
-		case 3:
-			clear3 = clear;
-			break;
-		case 4:
-			clear4 = clear;
-			break;
-		case 5:
-			clear5 = clear;
-			break;
-		case 6:
-			clear6 = clear;
-			break;
-		}
-	}
-	
-	public Clearing getClearing(int clear){
-		Clearing rClearing = null;
-		switch(clear){
-		case 1:
-			rClearing = clear1;
-			break;
-		case 2:
-			rClearing = clear2;
-			break;
-		case 3:
-			rClearing = clear3;
-			break;
-		case 4:
-			rClearing = clear4;
-			break;
-		case 5:
-			rClearing = clear5;
-			break;
-		case 6:
-			rClearing = clear6;
-			break;
-		}
-		return rClearing;
-	}
 	
 }
