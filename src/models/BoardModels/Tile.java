261215b3e7c119273339b0b8379e4206751505c5
Mitchell <Mitchell@172.17.69.76>
2015-03-17 15:11:43 -0700

    Tile update

--- src/models/BoardModels/Tile.java
+++ src/models/BoardModels/Tile.java
@@ -1,9 +1,12 @@
 package models.BoardModels;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 import models.characterModels.playerEnums.TileType;
+import models.chitModels.EnvironmentChit.SoundChit;
+import models.chitModels.EnvironmentChit.WarningChit;
 
 public class Tile {
 	
@@ -13,18 +16,22 @@ public class Tile {
 	 * and if its a mountains caves valley or forest
 	 */
 	
-	private Set <Clearing> clearings;
 	private TileType type;
+	private WarningChit warning;
+	private Set <Clearing> clearings;
+	private ArrayList<WarningChit> sounds;
 	
 	public Tile(){
 		type = null;
 		clearings = new HashSet<Clearing>();
+		this.sounds = new ArrayList<WarningChit>();
 	}
 	
 	// These can be null if not there
 	public Tile(TileType tile, Clearing ... clearings){
 		type = tile;
 		this.clearings = new HashSet<Clearing>();
+		this.sounds = new ArrayList<WarningChit>();
 		
 		// Add everything In
 		for (Clearing c: clearings) {
