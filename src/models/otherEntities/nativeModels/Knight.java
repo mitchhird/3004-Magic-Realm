f70e2072a169c5d96f0f94613885b267357858e3
Anthony Tasca <atasca10@gmail.com>
2015-03-22 18:09:40 -0400

    Fleshing out the knight and native models

--- src/models/otherEntities/nativeModels/Knight.java
+++ src/models/otherEntities/nativeModels/Knight.java
@@ -1,5 +1,16 @@
 package models.otherEntities.nativeModels;
 
-public class Knight extends NativeBase{
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class Knight extends NativeBase{
+	public Knight(){
+		super();
+		try {
+			nativeImage = ImageIO.read(getClass().getResource("/natives_c/knight_o.gif"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/models/otherEntities/nativeModels/NativeBase.java
+++ src/models/otherEntities/nativeModels/NativeBase.java
@@ -1,7 +1,14 @@
 package models.otherEntities.nativeModels;
 
+import java.awt.Image;
+
 import models.otherEntities.EntityBase;
 
 public class NativeBase extends EntityBase{
-
+	protected String nativeType;
+	protected Image nativeImage;
+	
+	public Image getImage(){
+		return nativeImage;
+	}
 }
