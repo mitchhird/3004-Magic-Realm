8db1436e78bffac388157000e72bd7ef1e9d4579
Anthony Tasca <atasca10@gmail.com>
2015-03-30 11:55:39 -0400

    added shell classes so project will compile

---
+++ src/networking/sendables/PlayerListUpdate.java
@@ -0,0 +1,20 @@
+package networking.sendables;
+
+import java.util.ArrayList;
+
+import models.characterModels.PlayerBase;
+import sun.org.mozilla.javascript.internal.ObjToIntMap.Iterator;
+
+public class PlayerListUpdate {
+
+	public PlayerListUpdate(ArrayList<PlayerBase> thePlayers) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrayList<PlayerBase> getThePlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ src/networking/threads/BaseThreads/PlayerListUpdate.java
@@ -0,0 +1,16 @@
+package networking.threads.BaseThreads;
+
+import java.util.ArrayList;
+
+import models.characterModels.PlayerBase;
+import sun.org.mozilla.javascript.internal.ObjToIntMap.Iterator;
+
+public class PlayerListUpdate {
+
+	public ArrayList<PlayerBase> getThePlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
